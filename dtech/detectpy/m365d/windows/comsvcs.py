dc08b3c21b940ed31b4d950bc5a3458d2d00624a
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-12-12 10:03:26 -0500

    Update comsvcs.py

--- dtech/detectpy/m365d/windows/comsvcs.py
+++ dtech/detectpy/m365d/windows/comsvcs.py
@@ -8,13 +8,13 @@ class Comsvcs:
         self.query_text = None
 
 
-    def delta_comsvcs_0001(self, kql_ago='1d'):
+    def pid_comsvcs_0001(self, kql_ago='1d'):
 
         self.query_text = f"""DeviceProcessEvents
             | where Timestamp >= ago({kql_ago})
             | where ProcessCommandLine has_all ('comsvcs', 'minidump')
             """
-        self.query_json = {"delta": "delta-comsvcs-0001",
+        self.query_json = {"delta": "pid--comsvcs-0001",
                            "title": "Comsvcs.dll Created Minidump of Process",
                            "query": self.query_text
         }
@@ -22,14 +22,14 @@ class Comsvcs:
         return self
 
 
-    def delta_comsvcs_0002(self, kql_ago='1d'):
+    def pid_comsvcs_0002(self, kql_ago='1d'):
 
         self.query_text = f"""DeviceProcessEvents
             | where Timestamp >= ago({kql_ago})
             | where ProcessCommandLine has_all ('comsvcs', '#24')
                 or ProcessCommandLine has_all ('comsvcs', '-24')
             """
-        self.query_json = {"delta": "dpid-comsvcs-0002",
+        self.query_json = {"delta": "pid--comsvcs-0002",
                            "title": "Comsvcs.dll Called MiniDumpW Function to Dump Process",
                            "query": self.query_text
         }
@@ -37,14 +37,14 @@ class Comsvcs:
         return self
 
 
-    def delta_comsvcs_0003(self, kql_ago='1d'):
+    def pid_comsvcs_0003(self, kql_ago='1d'):
 
         self.query_text = f"""DeviceFileEvents
             | where Timestamp >= ago({kql_ago})
             | where ActionType =~ 'FileCreated'
                 and InitiatingProcessCommandLine has 'comsvcs'
             """
-        self.query_json = {"delta": "dpid-comsvcs-0003",
+        self.query_json = {"delta": "pid--comsvcs-0003",
                            "title": "Comsvcs.dll Used to Create Dump File of Process",
                            "query": self.query_text
         }
