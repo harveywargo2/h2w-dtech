4cd100497c2bb53a58a863a1587b4753d113e719
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-12-12 10:03:37 -0500

    delta update

--- dtech/delta/delta.py
+++ dtech/delta/delta.py
@@ -2,8 +2,8 @@ from stix2 import (CustomObject, properties, utils, v21)
 from _extensions import delta_ExtensionDefinitionSMO
 
 
-_type = 'x-delta'
-@CustomObject('x-delta', [
+_type = 'delta'
+@CustomObject(_type, [
     ('type', properties.TypeProperty(_type, spec_version='2.1')),
     ('spec_version', properties.StringProperty(fixed='2.1')),
     ('id', properties.IDProperty(_type, spec_version='2.1')),
@@ -13,7 +13,6 @@ _type = 'x-delta'
     ('name', properties.StringProperty(required=True)),
     ('description', properties.StringProperty()),
     ('external_references', properties.ListProperty(v21.ExternalReference)),
-    ()
 ], extension_name=delta_ExtensionDefinitionSMO.id)
 class Delta(object):
     pass
